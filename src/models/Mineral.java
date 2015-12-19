7bdda4959e77cd5adbd28a194c65f9c75cb71964
Levi Paradis (ldp343) <ldp343@mail.usask.ca>
2015-12-19 14:23:19 -0600

    Creating initial model, waiting on more information from client.

---
+++ src/models/Mineral.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author Levi Paradis
+ *
+ */
+public class Mineral {
+	private String name;
+	private String colour;
+	
+	/**
+	 * 
+	 * @param name the name of the mineral
+	 * @param colour the most common colour of the mineral
+	 */
+	public Mineral (String name,String colour){
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
