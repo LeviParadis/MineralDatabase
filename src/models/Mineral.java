93aae701e75dd6e85c24846245dfa79076e4bbdd
Levi Paradis (ldp343) <ldp343@mail.usask.ca>
2015-12-19 15:02:11 -0600

    Adding testing for serialization

--- src/models/Mineral.java
+++ src/models/Mineral.java
@@ -25,6 +25,7 @@ public class Mineral implements Serializable {
 	public Mineral (String name,String colour){
 		
 	}
+	
 	/**
 	 * @param args
 	 */
@@ -34,6 +35,22 @@ public class Mineral implements Serializable {
 	public static void main(String[] args) {
 
 		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
 	}
 
 }
---
+++ src/models/Serializationtest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package models;
+
+import java.io.IOException;
+
+/**
+ * @author Levi Paradis
+ *
+ */
+public class Serializationtest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String fileName ="Mineral.ser";
+		Mineral flourite = new Mineral("flourite","purple");
+		
+		//serialize this to file
+		try{
+			models.SerializationUtil.serialize(flourite, fileName);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		Mineral flouriteFromFile = null;
+		try {
+			flouriteFromFile = (Mineral) models.SerializationUtil.deserialize(fileName);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("flourite object" + flourite);
+		System.out.println("new flourite from file" + flouriteFromFile);
+	}
+
+}
